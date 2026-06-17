9622827eae39f063748b4064ca3197414a38bc75
Erick Emiliano <54825317+ErickMUOSD@users.noreply.github.com>
2026-06-17 16:27:57 -0600

    remove useless config

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,6 @@ export default defineConfig({
 datasource: {
     // Load environment variables here
     url: process.env.DATABASE_URL, 
-},
-// Generator settings can also be moved here if necessary
-generator: {
-    name: 'client',
-    provider: 'prisma-client-js',
-},
+}
+
 });
